ded97aa5b56556727bbfbc85724e08c3e9e1fc6d
Nivedita Bhagat <navyabhaga01@gmail.com>
2023-07-08 22:16:35 +0530

    result_description

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_art_sense/controller/prediction_descriptions.dart';
 
 class ResultScreen extends StatelessWidget {
   final File? image;
@@ -9,33 +10,47 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     final String description = predictionDescriptions[predictions] ?? 'No description available';
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            Image.file(
-              image!,
-              height: 300,
-              width: 500,
-            ),
-            SizedBox(height: 20),
-            if (predictions.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  predictions,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              Image.file(
+                image!,
+                height: 300,
+                width: 500,
+              ),
+              SizedBox(height: 20),
+              if (predictions.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    predictions,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
-                  textAlign: TextAlign.center,
+                ),
+              
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  description,
+                  style: TextStyle(fontSize: 16, color: Colors.white),
                 ),
               ),
-          ],
+            ],
+          ),
         ),
       ),
     );
